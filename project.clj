0cdcb3549d1c47c3e601f48a09321a5a5f028560
rod <rod@pu-gh.com>
2012-01-28 20:10:38 +0000

    removed clojure-config from deps

--- project.clj
+++ project.clj
@@ -1,7 +1,6 @@
 
 (defproject bppm "0.0.1"
     :description "Pear Package Management Tool"
-    :dependencies [[org.clojure/clojure "1.3.0"]
-                   [clojure-config "1.0.5"]]
+    :dependencies [[org.clojure/clojure "1.3.0"]]
     :main bppm.core)
 
