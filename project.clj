42b62d40aded071833dfedf027925137f26cb00e
rod <rod@pu-gh.com>
2012-02-05 09:14:11 +0000

    bumped version to 0.2

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 
-(defproject boxuk.fruity "0.1.0"
+(defproject boxuk.fruity "0.2.0"
     :description "Pear Package Management Tool"
     :dependencies [[org.clojure/clojure "1.3.0"]
                    [cheshire "2.1.0"]
-                   [boxuk.versions "0.0.4"]
+                   [boxuk.versions "0.0.5"]
                    [boxuk.toolchain "0.0.2"]]
     :main boxuk.fruity.core
     :uberjar-name "fruity.jar")
