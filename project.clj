80ed5d0a03cadaa962af5698ac88b138d2dda0af
rod <rod@pu-gh.com>
2012-02-13 00:11:03 +0000

    extracted github code to jithub library

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
     :dependencies [[org.clojure/clojure "1.3.0"]
                    [cheshire "2.1.0"]
                    [clj-http "0.3.1"]
+                   [jithub "0.0.1"]
                    [boxuk.versions "0.1.0"]
                    [boxuk.toolchain "0.0.2"]]
     :main boxuk.fruity.core
--- src/boxuk/fruity/backend/github_downloads.clj
+++ src/boxuk/fruity/backend/github_downloads.clj
@@ -9,22 +9,10 @@
     (:use boxuk.fruity.backend
           boxuk.fruity.util
           clojure.java.shell
-          cheshire.core)
-    (:require [clojure.java.io :as io]
-              [clj-http.client :as http])
-    (:import [java.io File]
-             [java.net URLConnection]))
+          jithub.core)
+    (:require [jithub.repos.downloads :as dl]))
 
 ;; Downloads
-
-(defn- api-url
-    [{:keys [user name]}]
-    (format "https://api.github.com/repos/%s/%s/downloads" user name))
-
-(defn- downloads
-    "Returns downloads for the specified library"
-    [library]
-    (parse-stream (io/reader (api-url library)) true))
 
 (defn- download-version
     "Parses a version string from a download name, or nil"
@@ -35,65 +23,23 @@
     "Return all version numbers of current downloads"
     [library]
     (filter (complement nil?)
-            (map download-version (downloads library))))
+            (map download-version (dl/all))))
 
-;; Uploads
-
-(defn- upload-request-params
-    "Parameters for the first step in file upload"
-    [library file]
-    (let [name (.getName file)]
-        { :basic-auth (:basic-auth library)
-          :body (generate-string {
-              :name name
-              :size (.length file)
-              :description name
-              :content_type (URLConnection/guessContentTypeFromName name)
-          })
-        }))
-
-(defn- upload-request
-    "Fetches upload request parameters, for saving to S3"
-    [library file]
-    (http/post (api-url library)
-        (upload-request-params library file)))
-
-(defn- upload-s3-params
-    "Parameters for uploading to S3"
-    [file params]
-    (let [name (.getName file)]
-        (array-map 
-            :key (:path params)
-            :acl (:acl params)
-            :success_action_status "201"
-            :Filename (:name params)
-            :AWSAccessKeyId (:accesskeyid params)
-            :Policy (:policy params)
-            :Signature (:signature params)
-            :Content-Type (:mime_type params)
-            :file (slurp file))))
-
-(defn- upload-s3
-    "Upload a file to S3"
-    [file params]
-    (http/post (:s3_url params)
-        { :multipart (upload-s3-params file params) }))
-
-(defn- upload
-    "Upload a file to Github"
-    [library]
-    (let [file (File. (:archive library))
-          req (upload-request library file)
-          params (parse-string (:body req) true)]
-              (upload-s3 file params)))
+(defn- jithub-repo
+    "Create a jithub repo map for the library"
+    [{:keys [user name password]}]
+    { :user user
+      :name name
+      :password password })
 
 ;; Public
 
 (defmethod latest-version :github-downloads
     [library]
-    (let [versions (download-versions library)]
-        (if (empty? versions) "0.0.0"
-            (latest-version versions))))
+    (with-repo (jithub-repo library)
+        (let [versions (download-versions library)]
+            (if (empty? versions) "0.0.0"
+                (latest-version versions)))))
 
 (defmethod deploy :github-downloads
     [library tag]
