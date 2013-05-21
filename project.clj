a50868a6edc136bff7fd52538ddd0630df1b4ade
Rhodri Pugh <rod@pu-gh.com>
2013-05-21 09:28:10 +0100

    changed group and bumped version

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
 
-(defproject boxuk.fruity "0.2.0"
+(defproject boxuk/fruity "0.3.0"
     :description "Pear Package Management Tool"
     :dependencies [[org.clojure/clojure "1.3.0"]
                    [cheshire "2.1.0"]
-                   [boxuk.versions "0.1.0"]
-                   [boxuk.toolchain "0.0.2"]]
+                   [boxuk/versions "0.6.0"]
+                   [boxuk/toolchain "0.0.3"]]
     :main boxuk.fruity.core
     :plugins [[lein-bin "0.1.2"]])
 
